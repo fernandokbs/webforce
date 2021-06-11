8bd0c3af928f1c58923957fbefe1aa0f3edcadb1
Fernando <fernando93d@gmail.com>
2021-06-11 13:56:27 -0500

    Show index methods for products

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
 
 Route::middleware(['auth:sanctum', 'isAdmin'])->group(function() {
-    Route::get('/user', function() {
-        return "Logged";
-    });
+    Route::apiResource('products', ProductController::class)->except('index','show');
 }); 
 
+// Auth routes
 Route::post('login', [UserController::class, 'login']);
 Route::post('register',[UserController::class, 'register']);
+
+// Product path
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{product:slug}', [ProductController::class, 'show']);
