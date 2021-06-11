1e75f963d78c132930c3e5506aa7cdb79d1d5381
Fernando <fernando93d@gmail.com>
2021-06-11 11:11:59 -0500

    Generated products migration controller seeder ..

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,17 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Product;
 
-class DatabaseSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        Product::factory()->create();
+        //
     }
 }
