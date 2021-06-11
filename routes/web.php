64990ee9456fccf61ea23ced3407c520de41ce3d
Fernando <fernando93d@gmail.com>
2021-06-11 12:29:55 -0500

    Basic mount

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LandingController;
 
-Auth::routes();
-Route::redirect('admin', 'login');
-
 Route::get('/', [LandingController::class, 'index']);
-Route::get('/home', [HomeController::class, 'index'])->name('home');
